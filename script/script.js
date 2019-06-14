5251c7a7c805d155947cc3e8b68d6dcbb4d6b791
Edy <maslesaeldin@gmail.com>
2019-06-14 17:13:29 +0200

    final version

--- script/script.js
+++ script/script.js
@@ -1,14 +1,23 @@
-//For sliding two forms on startup
+//Fetching buttons
 let backBtn = document.getElementById("back");
-let loginDiv = document.getElementById('login');
-let forgotDiv = document.getElementById('forgot');
 let loginBtn = document.getElementById('login_btn');
+let forgotPassBtn = document.getElementById('forgot-password');
+
+//Container div for forms
+let loginFormDiv = document.getElementById('login');
+let tokenFormDiv = document.getElementById('token-form');
+let resetPassDiv = document.getElementById('reset-form')
 
 loginBtn.addEventListener('click', function(){
-    loginDiv.style.cssText = ' left: -1400px';
-    forgotDiv.style.cssText = 'right: 0;'
+    loginFormDiv.style.cssText = 'left: -1400px';
+    tokenFormDiv.style.cssText = 'right: 0;'
 })
 backBtn.addEventListener('click', function(){
-    loginDiv.style.cssText = 'left: 0px;';
-    forgotDiv.style.cssText = 'right: -1400px;';
+    loginFormDiv.style.cssText = 'left: 0px;';
+    tokenFormDiv.style.cssText = 'right: -1400px;';
+})
+
+forgotPassBtn.addEventListener('click', function(){
+    resetPassDiv.style.cssText = 'left: 0px';
+    loginFormDiv.style.cssText = 'left: 500px';
 })
